699958ddaeb826e193bc5d898d1484244b543bfd
Aleksander Eskilson <aeskilson@schnauzer.ittc.ku.edu>
2014-09-23 14:58:44 -0500

    more happy cats

--- js/Image.js
+++ js/Image.js
@@ -1,15 +1,17 @@
 function Image(num, ctx) {
-    var x = Math.floor(Math.random() * window.innerWidth);
-    var y = Math.floor(Math.random() * window.innerHeight);
-    var w = Math.floor(Math.random() * window.innerWidth);
-    var h = Math.floor(Math.random() * window.innerHeight);
-    var theta = Math.random() * 2 * Math.PI;
-    var img = document.getElementById("happyCat");
-    
-    ctx.beginPath();
-    ctx.save();
-    ctx.rotate(theta);
-    ctx.drawImage(img, x, y, w, h);
-    ctx.closePath();
-    ctx.restore();
+    for (i = 0; i < 100; i++) {
+        var x = Math.floor(Math.random() * window.innerWidth);
+        var y = Math.floor(Math.random() * window.innerHeight);
+        var w = Math.floor(Math.random() * window.innerWidth);
+        var h = Math.floor(Math.random() * window.innerHeight);
+        var theta = Math.random() * 2 * Math.PI;
+        var img = document.getElementById("happyCat");
+        
+        ctx.beginPath();
+        ctx.save();
+        ctx.rotate(theta);
+        ctx.drawImage(img, x, y, w, h);
+        ctx.closePath();
+        ctx.restore();
+    }
 }
