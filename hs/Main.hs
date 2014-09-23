c17545f6fb4b09aef77bbea94afd45d003345926
RyanGlScott <ryan.gl.scott@ku.edu>
2014-09-23 13:37:44 -0500

    Image examples

--- hs/Main.hs
+++ hs/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Criterion.Main (defaultMain, bench, nfIO)
+import Paths_blank_canvas_mark
 import Graphics.Blank
 
 -------------------------------------------------------------------------------
@@ -8,6 +9,7 @@ import Graphics.Blank
 import qualified CirclesRandomSize
 import qualified CirclesUniformSize
 import qualified FillText
+import qualified Image
 import qualified IsPointInPath
 import qualified MeasureText
 import qualified Rave
@@ -23,6 +25,7 @@ benchmarks :: [CanvasBenchmark]
 benchmarks = [ CirclesRandomSize.benchmark
              , CirclesUniformSize.benchmark
              , FillText.benchmark
+             , Image.benchmark
              , IsPointInPath.benchmark
              , MeasureText.benchmark
              , Rave.benchmark
@@ -32,12 +35,15 @@ benchSummaries :: [String]
 benchSummaries = [ CirclesRandomSize.summary
                  , CirclesUniformSize.summary
                  , FillText.summary
+                 , Image.summary
                  , IsPointInPath.summary
                  , MeasureText.summary
                  , Rave.summary
                  ]
 
 runBenchmark :: IO ()
-runBenchmark = blankCanvas 3000 $ \ ctx -> do
-    defaultMain $ map (\(b, s) -> bench s . nfIO $ b ctx) $ zip benchmarks benchSummaries
-    putStrLn "done"
+runBenchmark = do
+    dat <- getDataDir
+    blankCanvas 3000 { root = dat } $ \ ctx -> do
+        defaultMain $ map (\(b, s) -> bench s . nfIO $ b ctx) $ zip benchmarks benchSummaries
+        putStrLn "done"
