d5ec817268f702d0ae54bda33622984a1d03e90c
Jim Stanton <jim@jhstanton.com>
2014-09-24 02:33:43 -0500

    Added 'staticasteroids' bench, just wired polygons

---
+++ js/StaticAsteroids.js
@@ -0,0 +1,21 @@
+function StaticAsteroids(num, ctx) {
+    var x;
+    var y;
+    var nextPt = function(pt) {
+        var sign = Math.random() < 0.5 ? -1 : 1;
+        return pt + (sign * Math.random() * 15);
+    };
+    for(i = 0; i < num; i++){
+        x = Math.random() * window.innerWidth;
+        y = Math.random() * window.innerHeight;
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+        for(j=0; j < 10; j++){
+            x = nextPt(x);
+            y = nextPt(y);
+            ctx.lineTo(x, y);
+        }
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
