6758c63669d9c62ee578beeab8ef09f7d3d7b31c
Andy Gill <andygill@ku.edu>
2014-09-24 15:40:45 -0500

    Clear the screen between frames in asteroids

--- js/StaticAsteroids.js
+++ js/StaticAsteroids.js
@@ -5,6 +5,7 @@ function StaticAsteroids(num, ctx) {
         var sign = Math.random() < 0.5 ? -1 : 1;
         return pt + (sign * Math.random() * 15);
     };
+    ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
     for(i = 0; i < num; i++){
         x = Math.random() * window.innerWidth;
         y = Math.random() * window.innerHeight;
