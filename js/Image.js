ad98798793f3a4df7ae7419a405a1736fc1c84c3
Andy Gill <andygill@ku.edu>
2014-09-24 15:04:54 -0500

    Fixing Image to use continuations

--- js/Image.js
+++ js/Image.js
@@ -1,17 +1,33 @@
-function Image(num, ctx) {
-    for (i = 0; i < 100; i++) {
+function ImageMark(num, ctx) {  return function (done) {
+    var i = 0;
+
+    var loop = function () {
         var x = Math.floor(Math.random() * window.innerWidth);
         var y = Math.floor(Math.random() * window.innerHeight);
         var w = Math.floor(Math.random() * window.innerWidth);
         var h = Math.floor(Math.random() * window.innerHeight);
         var theta = Math.random() * 2 * Math.PI;
-        var img = document.getElementById("happyCat");
-        
-        ctx.beginPath();
-        ctx.save();
-        ctx.rotate(theta);
-        ctx.drawImage(img, x, y, w, h);
-        ctx.closePath();
-        ctx.restore();
+
+        var img = new Image();
+
+      	img.onload = function() {
+              ctx.beginPath();
+              ctx.save();
+              ctx.rotate(theta);
+              ctx.drawImage(img, x, y, w, h);
+              ctx.closePath();
+              ctx.restore();
+
+            	i++;
+            	if (i > 100) {
+            	    done();
+            	} else {
+            	    loop();
+            	}
+
+      	};
+	      img.onerror = function(e) { console.log(e); }
+        img.src = "../images/cc.gif"
     }
-}
+    loop();
+}}
