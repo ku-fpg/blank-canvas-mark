e691522af37d27cf4a4cbea0ffc2c2a560f6b621
Andy Gill <andygill@ku.edu>
2015-04-24 16:14:39 -0500

    adding reading from file

--- Diet.hs
+++ Diet.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE BangPatterns, DeriveDataTypeable, RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings, BangPatterns, DeriveDataTypeable, RecordWildCards #-}
 
 module Main where
 
+import Data.Text (Text)
 import Criterion.Analysis
 import Criterion.Monad
 import Criterion.Types
@@ -13,10 +14,36 @@ import Control.Monad.Trans.Except
 import Data.Aeson
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Vector as V
+import Control.Applicative
+import Data.Monoid
+import Control.Monad
+import System.IO (stdin)
+
+data SingleTest = SingleTest String [(Double,Int)]
+        deriving Show
+
+instance FromJSON SingleTest where
+ parseJSON (Object v) =
+    SingleTest
+           <$> v .: "name"
+           <*> fmap f (v .: "results")
+      where f res = [ (t,i) | (Just t,i) <- res `zip` [1..] ]
+ parseJSON _ = mzero
+
+
 
 main = do
    initializeTime -- you need this
+   inp <- BS.hGetContents stdin
+   let Just [SingleTest nm res] = decode inp
    let config = defaultConfig { verbosity = Verbose, reportFile = return "foo.html" }
+   r <- withConfig config $ do
+           r1 <- analyseOne 0 nm $ V.fromList $ [ measured { measTime = t, measIters = fromIntegral i } | (t,i) <- res ]
+           report [r1]
+           return r1
+   print "called"
+{-   
+   
    let m v i = measured { measTime = v * 1.01 * fromIntegral i, measIters = i,  measCpuTime = v + 1.0, measCycles = 1000 }
    let n v i = measured { measTime = v * sqrt (fromIntegral i), measIters = i }
    print "calling"
@@ -29,6 +56,7 @@ main = do
    print "called"
    print r
    BS.putStrLn $ encode r
+-}
 
 analyseOne :: Int -> String -> V.Vector Measured -> Criterion Report
 analyseOne i desc meas = do
